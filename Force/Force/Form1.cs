eda67ee6d5cb847274a773b1c4748aba1f7462a3
Aya Eyceoz <ayaballerina@gmail.com>
2019-05-22 14:13:28 -0400

    added comments

--- Force/Force/Form1.cs
+++ Force/Force/Form1.cs
@@ -7,12 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//Aya and Amelia
-//5.4.2019
+//Final Project
+//Aya Eyceoz and Amelia Ballingall
+//June 6th, 2019
 //Programming Honors Period 2
 //HackExeter 2019
 //Force Freeway
-namespace Force //This for is the main menu where you can go to different minigames
+//The theme for this Hackathon was force. We have created a world called Force Freeway, a freeway filled with force related minigames. 
+//Move the character to the desired minigame and explore Force Freeway!
+namespace Force 
+ //This form is the main menu that allows you to go to different minigames.
 {
     public partial class frmMainMenu : Form
     {
@@ -23,30 +27,35 @@ namespace Force //This for is the main menu where you can go to different miniga
 
         private void frmMainMenu_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Left) //moves the character
+            if (e.KeyCode == Keys.Left) //moves the character left 
             {
                 picChar.Left = picChar.Left - 10;
             }
-            if (e.KeyCode == Keys.Right)
+            if (e.KeyCode == Keys.Right) //moves the character right
             {
                 picChar.Left = picChar.Left + 10;
             }
-            if (e.KeyCode == Keys.Up)
+            if (e.KeyCode == Keys.Up) //moves the character up
             {
                 picChar.Top = picChar.Top - 10;
             }
-            if (e.KeyCode == Keys.Down)
+            if (e.KeyCode == Keys.Down) //moves the character down
             {
                 picChar.Top = picChar.Top + 10;
             }
-            if (picChar.Bounds.IntersectsWith(picmgAvoid.Bounds)) //visit different minigames (go to different forms)
+
+            //visit different minigames (go to different forms)
+            //if the character's bounds intersect with another picturebox (character goes to the building),
+            //it will close out of the current form and open the form related to that picturebox (or building)
+
+            if (picChar.Bounds.IntersectsWith(picmgAvoid.Bounds)) 
             {
-                mgAvoid obj1 = new mgAvoid();
-                obj1.Show();
+                mgAvoid obj1 = new mgAvoid(); //goes to Minigame Avoid Academy
+                obj1.Show(); //mg stands for minigame
                 this.Hide();
             }            if (picChar.Bounds.IntersectsWith(mgMaze.Bounds))
             {
-                Maze obj2 = new Maze();
+                Maze obj2 = new Maze(); //goes to Mze 
                 obj2.Show();
                 this.Hide();
             }
