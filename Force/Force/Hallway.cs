f7666dc9e07500a68315699d065952e7a4f7c978
Aya Eyceoz <ayaballerina@gmail.com>
2019-05-24 08:15:07 -0400

    hallway

--- Force/Force/Hallway.cs
+++ Force/Force/Hallway.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//this is a hallway that acts as a mini menu
+//it allows the user to go back to the main menu, or choose 1 player/2 player tug of war
 namespace Force
 {
     public partial class Hallway : Form
@@ -17,14 +18,14 @@ namespace Force
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) //brings back to main menu
         {
             frmMainMenu obj8 = new frmMainMenu();
             obj8.Show();
             this.Hide();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e) //brings to one player tug of war
         {
             TugOfWar obj9 = new TugOfWar();
             obj9.Show();
@@ -33,7 +34,7 @@ namespace Force
 
         private void button3_Click(object sender, EventArgs e)
         {
-            ToW2 obj83 = new ToW2();
+            ToW2 obj83 = new ToW2(); //brings to two player tug of war
             obj83.Show();
             this.Hide();
         }
